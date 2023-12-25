c09ea013c1a3678eeff018d3034c38f65ae43e3f
Gianfranco Cicciomessere <cicciomesere@gmail.com>
2023-12-25 13:19:08 +0000

    Music box sound effects

--- Scripts/Game/Objects/CajaDeMusica.gd
+++ Scripts/Game/Objects/CajaDeMusica.gd
@@ -3,21 +3,32 @@ extends Sprite2D
 signal isWinding(delta)
 
 var windProgress = 0
+var forwardSfxPos : float
 @export var winding = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(winding):
-		windProgress += delta * 100
 		emit_signal("isWinding", delta)
+		$Manivela.rotation_degrees = $Forward.get_playback_position() * 100
 	else:
-		if(windProgress > 0):
-			windProgress -= delta * 50
+		$Manivela.rotation_degrees = forwardSfxPos * 100
+		if forwardSfxPos > 0:
+			forwardSfxPos -= delta
+			if forwardSfxPos <= 0:
+				forwardSfxPos = 0
+				$Backwards.stop()
+				$Stop.play()
 	
-	$Manivela.rotation_degrees = windProgress * 2
 
 func _on_accion_button_down():
 	winding = true
+	$Backwards.stop()
+	$Forward.play()
+	$Forward.seek(forwardSfxPos)
 
 func _on_accion_button_up():
 	winding = false
+	forwardSfxPos = $Forward.get_playback_position()
+	$Forward.stop()
+	$Backwards.play()
